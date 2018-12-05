4e6ad90588c66a74142392c810a65793b5f00725
Andrew Huth <ahuth@mavenlink.com>
2018-12-05 13:12:20 -0800

    Fix lint errors

--- lib/input/input.spec.jsx
+++ lib/input/input.spec.jsx
@@ -3,15 +3,15 @@ import { shallow } from 'enzyme';
 import Input from './input';
 
 function render(props) {
-  return shallow(<Input {...props} id="hi" />)
+  return shallow(<Input {...props} id="hi" />);
 }
 
-test('rendering', function () {
+test('rendering', () => {
   const wrapper = render();
   expect(wrapper.type()).toEqual('input');
 });
 
-test('passing props through', function () {
+test('passing props through', () => {
   const wrapper = render({ className: 'hi', required: true });
   expect(wrapper.prop('className')).toEqual('hi');
   expect(wrapper.prop('required')).toEqual(true);
