080a4a961df5003c28a72b2e50f99af71c78e2c8
Andrew Huth <ahuth@mavenlink.com>
2018-12-06 15:49:30 -0800

    Fix lint errors in the babel config

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
-module.exports = function (api) {
+module.exports = function (api) { // eslint-disable-line import/no-commonjs
   api.cache(true);
 
   return {
     presets: [
-      "@babel/preset-react",
+      '@babel/preset-react',
       [
-        "@babel/preset-env",
+        '@babel/preset-env',
         {
-          "targets": {
-            "node": true,
+          targets: {
+            node: true,
           },
         },
       ],
