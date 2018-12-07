34539b2fe45a0e2e757e8e6d2263aa1cf9657e9f
Andrew Huth <ahuth@mavenlink.com>
2018-12-07 15:04:48 -0800

    Rename lib/ to src/

--- stories/input.jsx
+++ stories/input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Input from '../lib/input';
+import Input from '../src/input';
 
 storiesOf('Input', module)
   .add('Normal', () => <Input id="hello" />);
