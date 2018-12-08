d1286b5202255233e65240e1f689c44eeba7cbec
Andrew Huth <ahuth@mavenlink.com>
2018-12-08 09:47:04 -0800

    Move components and styles into their own directories
    I need a place to put test utils, and it probably makes sense to have everything organized in their own directories.

--- stories/input.jsx
+++ stories/input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Input from '../src/input';
+import Input from '../src/components/input';
 
 storiesOf('Input', module)
   .add('Normal', () => <Input id="hello" />);
