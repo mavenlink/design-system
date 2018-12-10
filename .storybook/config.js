96d6c78b8a9c56c171e10f40f8ced083a5c35eca
Andrew Huth <ahuth@mavenlink.com>
2018-12-10 11:36:24 -0800

    Customize storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+import { addDecorator, configure } from '@storybook/react';
 
 function loadStories() {
   require('../stories/index.js');
 }
+
+addDecorator(withOptions({
+  name: 'Mavenlink Design System',
+  url: 'https://mavenlink.design',
+  addonPanelInRight: true,
+}));
 
 configure(loadStories, module);
