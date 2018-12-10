d172148b5365ad6571e6eb4672cf4e1e615e8e97
Andrew Huth <ahuth@mavenlink.com>
2018-12-10 11:04:38 -0800

    Add accessibility addon and update Input example
    The Input example needed to be updated so that it would pass the accessibility checks.

--- stories/input.jsx
+++ stories/input.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { checkA11y } from '@storybook/addon-a11y';
 import { storiesOf } from '@storybook/react';
 import Input from '../src/components/input';
+import styles from './input.css';
 
 const stories = storiesOf('Input', module);
 
-stories.add('Normal', () => <Input id="hello" />);
+stories.addDecorator(checkA11y);
+stories.add('Normal', () => (
+  <div>
+    <label className={styles.label} htmlFor="greeting">Greeting</label>
+    <Input id="greeting" placeholder="hello world" />
+  </div>
+));
