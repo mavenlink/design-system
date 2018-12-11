4c9dcff00410a4df9f1c95864097e6de6fdce53a
Andrew Huth <ahuth@mavenlink.com>
2018-12-11 13:52:55 -0800

    Remove accessibility tab

--- stories/input.jsx
+++ stories/input.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { checkA11y } from '@storybook/addon-a11y';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import Input from '../src/components/input';
@@ -7,7 +6,6 @@ import styles from './input.css';
 
 const stories = storiesOf('Input', module);
 
-stories.addDecorator(checkA11y);
 stories.addDecorator(withKnobs);
 
 stories.add('Default', () => (
