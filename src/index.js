8e2ff4ce7da669060fc18219414b75cfbfbb3cdb
Andrew Huth <ahuth@mavenlink.com>
2018-12-10 16:33:06 -0800

    Revert "See what happens with a lint failure"
    This reverts commit 49bfddae8e483d6e953fadc8f53443a2148ca98a.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-export { default as Input } from './components/input' // eslint-disable-line import/prefer-default-export
+export { default as Input } from './components/input'; // eslint-disable-line import/prefer-default-export
