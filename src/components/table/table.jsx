b1006cde3f868ff2b112aae8ff02b0e0c10e42eb
Andrew <andrewhuth2@live.com>
2018-12-18 14:26:25 -0800

    Add RFC link to the table component

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -2,6 +2,10 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './table.css';
 
+/**
+ * Base table component.
+ * @see [RFC](https://github.com/mavenlink/rfc/pull/148)
+ */
 export default function Table({ children, className, ...rest }) {
   return (
     <table className={className} {...rest}>
