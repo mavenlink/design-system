729ab2a17d1bf2f16489c74541f82afcdd7cd86d
Andrew Huth <ahuth@mavenlink.com>
2018-12-17 17:55:54 -0800

    Move testutils to the /lib directory
    Makes it easy to exclude it from the NPM package.

--- src/components/input/__tests__/input.spec.jsx
+++ src/components/input/__tests__/input.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Input from '../input';
-import { createWrapper, findByType, getProp } from '../../../test-utils';
+import { createWrapper, findByType, getProp } from '../../../../lib/test-utils';
 
 function render(props) {
   return createWrapper(<Input {...props} id="hi" />);
