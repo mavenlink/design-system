4758531d7795643a4372a96ddaffe76317083186
Andrew <andrewhuth2@live.com>
2019-01-07 15:23:36 -0800

    Revert "Sanity check that test will fail on CI"
    This reverts commit 742a380a2fe116175f2df3a8000cdbd77634f5d5.

--- styleguide/cypress/integration/styleguide-spec.js
+++ styleguide/cypress/integration/styleguide-spec.js
@@ -1,6 +1,6 @@
 describe('styleguide', () => {
   it('works', () => {
     cy.visit('');
-    cy.get('header').contains('Design SystemZZZ');
+    cy.get('header').contains('Design System');
   });
 });
