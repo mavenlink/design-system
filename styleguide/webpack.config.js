a213c798bf651c69b7340e70d49d079b1868ecd4
Andrew <andrewhuth2@live.com>
2019-01-09 09:27:10 -0800

    Explain why things are the way they are

--- styleguide/webpack.config.js
+++ styleguide/webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
               ident: 'postcss',
               plugins: () => [
                 postCssCustomProperties({
+                  // TODO: Figure out how to remove the `importFrom` option below. We needed it because:
+                  //
+                  // 1. postcss-loader runs _before_ css-loader.
+                  // 2. css-loader has not resolved imports, yet.
+                  // 3. postcss doesn't know about the CSS variable definitions.
+                  //
+                  // One possible approach to resolving this is to remove css-loader and use postcss-import
+                  // and postcss-modules instead.
                   importFrom: [
                     path.join(__dirname, '../src/styles/colors.css'),
                     path.join(__dirname, '../src/styles/typography.css'),
