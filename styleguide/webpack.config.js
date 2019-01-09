6b6a3d6406f19886ca0b2d1cde717d69f4736e55
Andrew <andrewhuth2@live.com>
2019-01-09 10:29:09 -0800

    Explain why things are the way the are, again
    Did this once before in a213c798bf651c69b7340e70d49d079b1868ecd4, before trying out 1d742e4446a861359a1dfab5649701cd5d125043 and then reverting it in c36ce551090fb25f455821a32219a8081a8c4d67.

--- styleguide/webpack.config.js
+++ styleguide/webpack.config.js
@@ -31,6 +31,14 @@ module.exports = {
                   stage: false,
                   features: {
                     'custom-properties': {
+                      // TODO: Figure out how to get rid of the `importFrom` option. It's needed because:
+                      //
+                      // 1. postcss-loader runs before css-loader.
+                      // 2. css-loader therefore hasn't processed the CSS imports, yet.
+                      // 3. postcss-loader doesn't know about imported files when backfilling fallbacks for css variables.
+                      //
+                      // I've investigated resolving this by using postcss-import to process CSS imports. It works, but ends
+                      // duplicating imported files. See c36ce551090fb25f455821a32219a8081a8c4d67.
                       importFrom: [
                         path.join(__dirname, '../src/styles/colors.css'),
                         path.join(__dirname, '../src/styles/typography.css'),
