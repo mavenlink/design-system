1d742e4446a861359a1dfab5649701cd5d125043
Andrew <andrewhuth2@live.com>
2019-01-09 09:52:59 -0800

    Use postcss to process CSS imports
    Instead of css-loader. css-loader does a fine job, but I need the imports to be resolved **_before_** postcss processes and backfills css variables, so that it will know about variable definitions from imported stylesheets.

--- styleguide/webpack.config.js
+++ styleguide/webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const postCssCustomProperties = require('postcss-custom-properties');
+const postCssImport = require('postcss-import');
 
 module.exports = {
   module: {
@@ -21,26 +22,14 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader', options: { importLoaders: 1, modules: true } },
+          { loader: 'css-loader', options: { import: false, importLoaders: 1, modules: true } },
           {
             loader: 'postcss-loader',
             options: {
               ident: 'postcss',
               plugins: () => [
-                postCssCustomProperties({
-                  // TODO: Figure out how to remove the `importFrom` option below. We needed it because:
-                  //
-                  // 1. postcss-loader runs _before_ css-loader.
-                  // 2. css-loader has not resolved imports, yet.
-                  // 3. postcss doesn't know about the CSS variable definitions.
-                  //
-                  // One possible approach to resolving this is to remove css-loader and use postcss-import
-                  // and postcss-modules instead.
-                  importFrom: [
-                    path.join(__dirname, '../src/styles/colors.css'),
-                    path.join(__dirname, '../src/styles/typography.css'),
-                  ],
-                }),
+                postCssImport,
+                postCssCustomProperties,
               ],
             },
           },
