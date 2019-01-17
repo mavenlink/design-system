256fd43994e16575ab93410df66ba55f77f2e81c
Andrew <ahuth@users.noreply.github.com>
2019-01-16 16:23:01 -0800

    Update Google Analytics tracking number

--- styleguide/content/utils/page-view.js
+++ styleguide/content/utils/page-view.js
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('UA-7529271-29');
+ReactGA.initialize('UA-7529271-31');
 
 function convertToTrackableUrlForGoogle(url) {
   return url.replace(/\/(?:design-system)?#!/, '');
