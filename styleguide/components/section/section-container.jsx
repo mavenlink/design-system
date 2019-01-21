38f82751c87a15a4b179b7fa9f739834ee6c6807
Rob Levin <roblevintennis@gmail.com>
2019-01-21 14:19:28 -0800

    disable import/extensions for rsg component imports

--- styleguide/components/section/section-container.jsx
+++ styleguide/components/section/section-container.jsx
@@ -1,5 +1,5 @@
-import SectionHeading from 'rsg-components/SectionHeading';
-import Markdown from 'rsg-components/Markdown';
+import SectionHeading from 'rsg-components/SectionHeading'; // eslint-disable-line import/extensions
+import Markdown from 'rsg-components/Markdown'; // eslint-disable-line import/extensions
 import React from 'react';
 import Section from './section';
 
