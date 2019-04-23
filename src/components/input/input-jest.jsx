a53e47711526e25d0992a9b4594d894c191e2a9b
Rob Levin <roblevintennis@gmail.com>
2019-04-23 15:44:20 -0700

    Fix some lint issues

--- src/components/input/input-jest.jsx
+++ src/components/input/input-jest.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Input from './input.jsx';
+import Input from './input';
 
-describe('Input', function() {
+describe('Input', () => {
   it('renders', () => {
     const props = { className: 'klass', required: true };
     const tree = renderer
-      .create(<Input { ...props } id='foo' />)
+      .create(<Input {...props} id="foo" />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
