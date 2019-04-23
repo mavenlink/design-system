0d9f637c9f574e8e1ad88c07e605abce9c9b11cf
Rob Levin <roblevintennis@gmail.com>
2019-04-23 15:48:03 -0700

    Remove spec that's now covered by having a jest snapshot

--- src/components/input/input.spec.jsx
+++ src/components/input/input.spec.jsx
@@ -6,11 +6,6 @@ function render(props) {
   return shallow(<Input {...props} id="hi" />);
 }
 
-test('rendering', () => {
-  const wrapper = render();
-  expect(wrapper.find('input').prop('id')).toEqual('hi');
-});
-
 test('passing props through', () => {
   const wrapper = render({ className: 'hello', required: true });
   expect(wrapper.find('input').prop('className')).toEqual('hello');
