374f03498608bf4b44fc90e234aef45dc2a2de35
Juan Carlos Medina <juanca@mavenlink.com>
2019-07-12 11:58:39 -0700

    Implement button without styling
    I am playing around with the idea of providing a color from our stylesheet as a prop. I wonder if this is better if others pass down the className with the appropriate color? Hmmmm...

---
+++ src/components/button/button.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from './button.css';
+
+export default function Button(props) {
+  const {
+    children,
+    className,
+    color,
+    type,
+    ...buttonElemProps
+  } = props;
+
+  return (
+    <button
+      {...buttonElemProps}
+      className={className || styles[color]}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  color: PropTypes.oneOf(['primary', 'secondary']),
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['button', 'reset', 'submit'])
+};
+
+Button.defaultProps = {
+  className: styles.primary,
+  color: 'primary',
+  type: undefined,
+};
---
+++ src/components/button/button.md
@@ -0,0 +1,11 @@
+```jsx
+<Button>Default Button</Button>
+```
+
+```jsx
+<Button color="primary">Primary Button</Button>
+```
+
+```jsx
+<Button color="secondary">Secondary Button</Button>
+```
