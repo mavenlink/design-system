c1326c139db19248a52b3d0b12197c4504da9ffa
Juan Carlos Medina <juanca@mavenlink.com>
2019-07-23 18:06:55 -0700

    Support CSS modules syntax via Stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-css-modules',
   ],
   plugins: [
   ],
   rules: {
     'no-descending-specificity': null,
-    'property-no-unknown': null, // TODO: We need to support CSS modules
-    'selector-pseudo-class-no-unknown': null, // TODO: We need to support CSS modules
   },
 };
