b03352f8b8db81aef42dc6aa1410637ce68ec077
Rob Levin <roblevintennis@gmail.com>
2019-08-13 14:41:52 -0700

    Fix lint issues

--- src/components/file-picker/file-picker.test.jsx
+++ src/components/file-picker/file-picker.test.jsx
@@ -1,62 +1,63 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import FilePicker from './file-picker';
 import styles from './file-picker.css';
 
 describe('FilePicker', () => {
   describe('API', () => {
-    it('title', function () {
+    it('title', () => {
       const expected = 'Attach Le Files';
-      const { getByText } = render(<FilePicker id="123" title={ expected } />);
+      const { getByText } = render(<FilePicker id="123" title={expected} />);
       expect(getByText(expected).textContent).toContain(expected);
     });
 
-    it('multiple', function () {
+    it('multiple', () => {
       const testRenderer = renderer.create(
-        <FilePicker multiple='multiple' id="123" title='yo' />
+        <FilePicker multiple="multiple" id="123" title="yo" />,
       );
       const testInstance = testRenderer.root;
       expect(testInstance.findByType('input').props.multiple).toBe('multiple');
     });
 
     describe('class prop defaults', () => {
-      let testInstance, testRenderer;
+      let testInstance;
+      let testRenderer;
 
-      beforeEach(function () {
+      beforeEach(() => {
         testRenderer = renderer.create(
-          <FilePicker id="123" title='yo' />
+          <FilePicker id="123" title="yo" />,
         );
         testInstance = testRenderer.root;
       });
 
-      afterEach(function () {
+      afterEach(() => {
         testRenderer = null;
         testInstance = null;
       });
 
-      it('dropzoneClasses', function () {
+      it('dropzoneClasses', () => {
         const section = testInstance.findByProps({
           className: styles.dropzone,
         });
         expect(section.type === 'section').toBe(true);
       });
 
-      it('labelClasses', function () {
+      it('labelClasses', () => {
         const label = testInstance.findByProps({
           className: styles.label,
         });
         expect(label.type === 'label').toBe(true);
       });
 
-      it('fileClasses', function () {
+      it('fileClasses', () => {
         const file = testInstance.findByProps({
           className: styles.file,
         });
         expect(file.type === 'input').toBe(true);
       });
 
-      it('fileListClasses', function () {
+      it('fileListClasses', () => {
         const fileList = testInstance.findByProps({
           className: styles['file-list'],
         });
@@ -69,49 +70,50 @@ describe('FilePicker', () => {
       const expectedLabelClasses = 'le-label';
       const expectedFileClasses = 'le-file';
       const expectedFileListClasses = 'le-filelist';
-      let testInstance, testRenderer;
+      let testInstance;
+      let testRenderer;
 
-      beforeEach(function () {
+      beforeEach(() => {
         testRenderer = renderer.create(
           <FilePicker
-            dropzoneClasses={ expectedDropzoneClasses }
-            labelClasses={ expectedLabelClasses }
-            fileClasses={ expectedFileClasses }
-            fileListClasses={ expectedFileListClasses }
+            dropzoneClasses={expectedDropzoneClasses}
+            labelClasses={expectedLabelClasses}
+            fileClasses={expectedFileClasses}
+            fileListClasses={expectedFileListClasses}
             id="123"
-            title='yo'
-          />
+            title="yo"
+          />,
         );
         testInstance = testRenderer.root;
       });
 
-      afterEach(function () {
+      afterEach(() => {
         testRenderer = null;
         testInstance = null;
       });
 
-      it('dropzoneClasses', function () {
+      it('dropzoneClasses', () => {
         const section = testInstance.findByProps({
           className: expectedDropzoneClasses,
         });
         expect(section.type === 'section').toBe(true);
       });
 
-      it('labelClasses', function () {
+      it('labelClasses', () => {
         const label = testInstance.findByProps({
           className: expectedLabelClasses,
         });
         expect(label.type === 'label').toBe(true);
       });
 
-      it('fileClasses', function () {
+      it('fileClasses', () => {
         const fileInput = testInstance.findByProps({
           className: expectedFileClasses,
         });
         expect(fileInput.type === 'input').toBe(true);
       });
 
-      it('fileListClasses', function () {
+      it('fileListClasses', () => {
         const fileListSection = testInstance.findByProps({
           className: expectedFileListClasses,
         });
