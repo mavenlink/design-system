68ee789718372a7284ceb21a8dbdfefd5e8eb268
Rob Levin <roblevintennis@gmail.com>
2019-08-20 13:58:50 -0700

    Favor using an override directive from top-level .eslintrc for each of finding rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,21 @@ module.exports = {
     rules: {
       'import/no-commonjs': 'off',
     },
+  }, {
+    files: [
+      '**/file-picker/file-picker*',
+    ],
+    rules: {
+      // Override: role button for file-picker's label allows it to
+      // behave as a file dialog open button in my VoiceOver testing
+      'jsx-a11y/no-noninteractive-element-to-interactive-role': [
+        'error',
+        {
+          label: [
+            'button',
+          ],
+        },
+      ],
+    },
   }],
 };
