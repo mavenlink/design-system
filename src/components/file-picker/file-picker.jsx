5e5501bf74078bd9490269b0894a0946e5f93f63
Rob Levin <roblevintennis@gmail.com>
2019-08-20 14:14:01 -0700

    Fix some lint issues

--- src/components/file-picker/file-picker.jsx
+++ src/components/file-picker/file-picker.jsx
@@ -95,13 +95,13 @@ const FilePicker = (props) => {
 
   const getFilesList = () => {
     if (files.length) {
-      const listItems = files.map((file) =>
+      const listItems = files.map(file => (
         <li className={styles['file-list-button']} key={file.name}>
           <Icon className={styles.icon} name={iconFileDefault.id} size="medium" stroke="grey-base" fill="none" title="Upload file icon" />
           <span className={styles.filename}>{file.name}</span>
           <button onClick={e => onRemoveFile(e, file)} className={styles.remove}>&times;</button>
         </li>
-      );
+      ));
       return (
         <ul className={styles['file-ul']}>{listItems}</ul>
       );
