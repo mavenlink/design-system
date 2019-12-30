dc400477a5668c566887c396d23de16bf45b6c28
Juan-Carlos Medina <juanca@mavenlink.com>
2019-12-30 15:14:24 -0600

    Update Icon tests to use `@testing-library/react` patterns
    I also took the opportunity to loop unroll tests. This is more compatible with the new testing patterns since it renders to the DOM.
    
    I also took the opportunity to be more explicit in the tests and avoid asserting other states of the components which were unrelated to the actual test -- the default classes are already tested in the snapshot!

--- src/components/icon/icon.test.jsx
+++ src/components/icon/icon.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Icon from './icon.jsx';
 
 describe('Icon', () => {
@@ -11,78 +12,144 @@ describe('Icon', () => {
   });
 
   it('has title', () => {
-    const tree = renderer.create((
+    render((
       <Icon name="foobar" title="yo" />
-    )).root;
-    expect(tree.findByType('title').props.children).toBe('yo');
+    ));
+    expect(screen.getByRole('img')).toContainHTML('<title>yo</title>');
   });
 
   describe('size API', () => {
-    it('renders small, medium, and large sizes', () => {
-      const sizes = ['small', 'medium', 'large'];
-      sizes.forEach((size) => {
-        const tree = renderer.create((
-          <Icon name="foobar" size={size} />
-        )).toJSON();
-        expect(tree.props.className).toContain(`size-${size}`);
-      });
+    it('can be "small"', () => {
+      render((
+        <Icon name="foobar" size="small" />
+      ));
+      expect(screen.getByRole('img')).toHaveClass('size-small');
+    });
+
+    it('can be "medium"', () => {
+      render((
+        <Icon name="foobar" size="medium" />
+      ));
+      expect(screen.getByRole('img')).toHaveClass('size-medium');
+    });
+
+    it('can be "large"', () => {
+      render((
+        <Icon name="foobar" size="large" />
+      ));
+      expect(screen.getByRole('img')).toHaveClass('size-large');
     });
   });
 
   describe('className API', () => {
     describe('fill', () => {
-      it('sets all fill colors', () => {
-        const colors = ['primary', 'action', 'highlight', 'caution'];
-        colors.forEach((color) => {
-          const tree = renderer.create((
-            <Icon name="foobar" fill={color} />
-          )).toJSON();
-          expect(tree.props.className).toContain('icon-base');
-          expect(tree.props.className).toContain(`fill-${color}`);
-        });
+      it('can be "primary"', () => {
+        render((
+          <Icon name="foobar" fill="primary" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('fill-primary');
+      });
+
+      it('can be "action"', () => {
+        render((
+          <Icon name="foobar" fill="action" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('fill-action');
+      });
+
+      it('can be "highlight"', () => {
+        render((
+          <Icon name="foobar" fill="highlight" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('fill-highlight');
+      });
+
+      it('can be "caution"', () => {
+        render((
+          <Icon name="foobar" fill="caution" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('fill-caution');
+      });
+
+      it('can be "none"', () => {
+        render((
+          <Icon name="foobar" fill="none" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('fill-none');
       });
     });
 
     describe('stroke', () => {
-      it('sets stroke colors turning off fills and color', () => {
-        const colors = ['primary', 'action', 'highlight', 'caution'];
-        colors.forEach((color) => {
-          const tree = renderer.create((
-            <Icon name="foobar" stroke={color} />
-          )).toJSON();
-          expect(tree.props.className).toContain('icon-base');
-          expect(tree.props.className).toContain(`stroke-${color}`);
-          expect(tree.props.className).toContain('fill-none');
-          expect(tree.props.className).toContain('color-transparent');
-        });
+      it('can be "primary"', () => {
+        render((
+          <Icon name="foobar" stroke="primary" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('stroke-primary');
+      });
+
+      it('can be "action"', () => {
+        render((
+          <Icon name="foobar" stroke="action" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('stroke-action');
+      });
+
+      it('can be "highlight"', () => {
+        render((
+          <Icon name="foobar" stroke="highlight" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('stroke-highlight');
+      });
+
+      it('can be "caution"', () => {
+        render((
+          <Icon name="foobar" stroke="caution" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('stroke-caution');
+      });
+
+      it('can be "none"', () => {
+        render((
+          <Icon name="foobar" stroke="none" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('stroke-none');
       });
     });
 
     describe('currentColor', () => {
-      it('sets all font colors', () => {
-        const colors = ['primary', 'action', 'highlight', 'caution'];
-        colors.forEach((color) => {
-          const tree = renderer.create((
-            <Icon name="foobar" currentColor={color} />
-          )).toJSON();
-          expect(tree.props.className).toContain('icon-base');
-          expect(tree.props.className).toContain(`color-${color}`);
-        });
+      it('can be "primary"', () => {
+        render((
+          <Icon name="foobar" currentColor="primary" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('color-primary');
       });
-    });
 
-    describe('combining colors', () => {
-      it('can combine fill, stroke, and currentColor', () => {
-        const colors = ['primary', 'action', 'highlight', 'caution'];
-        colors.forEach((color) => {
-          const tree = renderer.create((
-            <Icon name="foobar" fill={color} stroke={color} currentColor={color} />
-          )).toJSON();
-          expect(tree.props.className).toContain('icon-base');
-          expect(tree.props.className).toContain(`fill-${color}`);
-          expect(tree.props.className).toContain(`color-${color}`);
-          expect(tree.props.className).toContain(`stroke-${color}`);
-        });
+      it('can be "action"', () => {
+        render((
+          <Icon name="foobar" currentColor="action" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('color-action');
+      });
+
+      it('can be "highlight"', () => {
+        render((
+          <Icon name="foobar" currentColor="highlight" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('color-highlight');
+      });
+
+      it('can be "caution"', () => {
+        render((
+          <Icon name="foobar" currentColor="caution" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('color-caution');
+      });
+
+      it('can be "transparent"', () => {
+        render((
+          <Icon name="foobar" currentColor="transparent" />
+        ));
+        expect(screen.getByRole('img')).toHaveClass('color-transparent');
       });
     });
   });
