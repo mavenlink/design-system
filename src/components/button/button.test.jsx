232aa07007badd10a7765facc7dce8ae9e5e00a9
Juan-Carlos Medina <juanca@mavenlink.com>
2019-12-31 13:55:40 -0600

    Alphabetize

--- src/components/button/button.test.jsx
+++ src/components/button/button.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Button from './button.jsx';
 
 describe('Button', () => {
