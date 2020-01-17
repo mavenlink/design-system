829c0299ae9d994cf59acbd2f4ea8f3b3ddd102f
Juan-Carlos Medina <juanca@mavenlink.com>
2020-01-16 18:05:09 -0800

    Remove unused file

--- setup-enzyme.js
+++
@@ -1,9 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import '@testing-library/jest-dom/extend-expect.js';
-
-configure({ adapter: new Adapter() });
-
-console.error = (message) => { // eslint-disable-line
-  throw new Error(message);
-};
