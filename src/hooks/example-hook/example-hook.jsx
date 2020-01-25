f1e273ce4ac9f100753c4f485b5521dc47ffb9af
Juan-Carlos Medina <juanca@mavenlink.com>
2020-01-24 20:30:22 -0800

    Implement most basic hook as an example

---
+++ src/hooks/example-hook/example-hook.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+// This exists to satisfy Styleguidist, don't use it!
+export function ExampleHook() {
+  return (
+    <div />
+  );
+}
+
+export default function useExampleHook(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  return [value, setValue];
+}
