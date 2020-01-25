24e6547da66cf188db4022ea8cf961306bd109b5
Juan-Carlos Medina <juanca@mavenlink.com>
2020-01-24 20:09:55 -0800

    Add helpful comment

--- setup-test-framework.js
+++ setup-test-framework.js
@@ -1,5 +1,9 @@
 import '@testing-library/jest-dom/extend-expect.js';
 
 console.error = (message) => { // eslint-disable-line
+  // During test development, it might be useful to comment out
+  // this error handling. It is useful for ensuring our tests
+  // do not violate propTypes but it can make stack traces
+  // much harder to understand.
   throw new Error(message);
 };
