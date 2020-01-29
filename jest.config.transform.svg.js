a297114d2ef7b2a29e075fe1ab5bcd13e17ec467
Juan-Carlos Medina <juanca@mavenlink.com>
2020-01-28 16:42:39 -0800

    Process SVGs a little differently
    Looks like we cannot tell which SVG was being used. Now it uses the filename in the tests. Unfortunately, using a custom tranform implies that we need to specify the JS and JSX transforms.

---
+++ jest.config.transform.svg.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  process(src, filename, config, options) {
+    // import iconCautionSvg from 'src/svgs/icon-caution-fill.svg';
+    // results in
+    // iconCautionSvg := { id: 'icon-caution-fill.svg' }
+    // which helps us assert we are using the appropriate SVG(s) in our components
+    return 'module.exports = { id: ' + JSON.stringify(path.basename(filename)) + ' };';
+  },
+};
