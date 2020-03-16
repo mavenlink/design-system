656d61e0b98f050c6f47fc5fefe18b4698d47d2f
Andre LaFleur <alafleur@mavenlink.com>
2020-03-16 12:29:23 -0600

    Curse you lint!

--- src/components/custom-field-input-text/custom-field-input-text.test.jsx
+++ src/components/custom-field-input-text/custom-field-input-text.test.jsx
@@ -203,20 +203,20 @@ describe('CustomFieldInputText', () => {
 
   describe('icon API', () => {
     it('shows an icon when provided', () => {
-      const icon = <Icon name={calendarSvg.id} currentColor="action"/>;
-      const {getByRole} = render(<TestComponent icon={icon}/>);
+      const icon = <Icon name={calendarSvg.id} currentColor="action" />;
+      const { getByRole } = render(<TestComponent icon={icon} />);
       expect(getByRole('img')).toBeDefined();
     });
 
     it('gives preference to the error icon', () => {
-      const icon = <Icon name={calendarSvg.id} currentColor="action" title="Hello"/>;
-      const {queryByTitle, getByRole} = render(<TestComponent icon={icon} error/>);
+      const icon = <Icon name={calendarSvg.id} currentColor="action" title="Hello" />;
+      const { queryByTitle, getByRole } = render(<TestComponent icon={icon} error />);
       expect(queryByTitle('Hello')).toBeNull();
       expect(getByRole('img').firstChild).toHaveAttribute('xlink:href', '#icon-caution-fill.svg');
     });
 
     it('shows no icon by default', () => {
-      const {queryByRole} = render(<TestComponent/>);
+      const { queryByRole } = render(<TestComponent />);
       expect(queryByRole('img')).toBeNull();
     });
   });
