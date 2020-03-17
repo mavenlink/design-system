da6202399ef3f698ae1d1dd15a7e1af701172d0c
Andre LaFleur <alafleur@mavenlink.com>
2020-03-17 15:07:56 -0600

    Begin extraction

--- src/components/helpers/input.jsx
+++ src/components/helpers/input.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Uncontrolled from './uncontrolled-input/uncontrolled-input.jsx';
-import Controlled from './controlled-input/controlled-input.jsx';
 
 export default function Input(props) {
+  const sharedProps = {
+    className: props.className,
+    disabled: props.disabled,
+    id: props.id,
+    onBlur: props.onBlur,
+    onChange: props.onChange,
+    onFocus: props.onFocus,
+    placeholder: props.placeholder,
+    ref: props.inputRef,
+    type: props.type,
+  };
+
   if (props.controlled) {
-    return (<Controlled {...props} />);
+    return (<input data-testid="private-input" value={props.value} {...sharedProps} />);
   }
 
-  return (<Uncontrolled {...props} />);
+  return (<input data-testid="private-input" {...sharedProps} />);
 }
 
 Input.propTypes = {
@@ -22,6 +32,7 @@ Input.propTypes = {
   onBlur: PropTypes.func,
   onChange: PropTypes.func,
   onFocus: PropTypes.func,
+  placeholder: PropTypes.string,
   type: PropTypes.oneOf(['date', 'number', 'text']),
   value: PropTypes.string,
 };
@@ -36,6 +47,7 @@ Input.defaultProps = {
   onBlur: () => {},
   onChange: () => {},
   onFocus: () => {},
+  placeholder: undefined,
   type: 'text',
   value: '',
 };
--- src/components/helpers/input.test.jsx
+++ src/components/helpers/input.test.jsx
@@ -1,34 +1,96 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import React, { createRef } from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import Input from './input.jsx';
 
 describe('src/components/helpers/input', () => {
   const renderComponent = (props = {}) => render(<Input id="foo" {...props} />);
-  const testId = 'controlled input';
+  const testId = 'private-input';
 
   afterEach(cleanup);
 
-  describe('controlled/uncontrolled API', () => {
-    it('respects the controlled setting', () => {
-      const { getByTestId } = renderComponent({
-        controlled: true,
-        type: 'number',
-        min: '1',
-        max: '10',
-      });
-      expect(getByTestId(testId)).not.toHaveAttribute('min', '1');
-      expect(getByTestId(testId)).not.toHaveAttribute('max', '10');
-    });
-
-    it('respects the uncontrolled setting', () => {
-      const { getByTestId } = renderComponent({
-        controlled: false,
-        type: 'number',
-        min: '1',
-        max: '10',
-      });
-      expect(getByTestId(testId)).toHaveAttribute('min', '1');
-      expect(getByTestId(testId)).toHaveAttribute('max', '10');
+  describe('className API', () => {
+    it('accepts a className prop', () => {
+      const { getByTestId } = renderComponent({ className: 'bar' });
+      expect(getByTestId(testId)).toHaveClass('bar');
     });
   });
+
+  describe('disabled API', () => {
+    it('can be enabled', () => {
+      const { getByTestId } = renderComponent({ disabled: false });
+      expect(getByTestId(testId)).not.toBeDisabled();
+    });
+
+    it('can be disabled', () => {
+      const { getByTestId } = renderComponent({ disabled: true });
+      expect(getByTestId(testId)).toBeDisabled();
+    });
+  });
+
+  describe('id API', () => {
+    it('sets the ID attribute', () => {
+      const { getByTestId } = renderComponent();
+      expect(getByTestId(testId)).toHaveAttribute('id', 'foo');
+    });
+  });
+
+  describe('inputRef API', () => {
+    it('sets the ref on itself', () => {
+      const inputRef = createRef();
+      const { getByTestId } = renderComponent({ inputRef });
+      expect(getByTestId(testId)).toBe(inputRef.current);
+    });
+  });
+
+  describe('type API', () => {
+    const types = ['text', 'date', 'number'];
+
+    types.forEach((type) => {
+      it(`accepts '${type}' as a valid type`, () => {
+        const { getByTestId } = renderComponent({ type });
+        expect(getByTestId(testId)).toHaveAttribute('type', type);
+      });
+    });
+  });
+
+  describe('interaction API', () => {
+    it('accepts onBlur', () => {
+      const onBlur = jest.fn();
+      const { getByTestId } = renderComponent({ onBlur });
+      fireEvent.blur(getByTestId(testId));
+      expect(onBlur.mock.calls.length).toBe(1);
+    });
+
+    it('accepts onChange', () => {
+      const onChange = jest.fn();
+      const { getByTestId } = renderComponent({ onChange });
+      fireEvent.change(getByTestId(testId), { target: { value: 'hello' } });
+      expect(onChange.mock.calls.length).toBe(1);
+    });
+
+    it('accepts onFocus', () => {
+      const onFocus = jest.fn();
+      const { getByTestId } = renderComponent({ onFocus });
+      fireEvent.focus(getByTestId(testId));
+      expect(onFocus.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('placeholder API', () => {
+    it('accepts a placeholder', () => {
+      const { getByTestId } = renderComponent({ placeholder: 'bar' });
+      expect(getByTestId(testId)).toHaveAttribute('placeholder', 'bar');
+    });
+  });
+
+  describe('when controlled', () => {
+    describe('value API', () => {
+      it('accepts a starting value', () => {
+        const { getByTestId } = renderComponent({ value: 'hello' });
+        expect(getByTestId(testId)).toHaveValue('hello');
+      });
+    });
+  });
+
+  // describe('when uncontrolled', () => {});
 });
