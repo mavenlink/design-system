4e4c7a3e9fe160065a3a5f9e0dce96c751e5e8dd
Andre LaFleur <alafleur@mavenlink.com>
2020-03-17 09:40:56 -0600

    Setup uncontrolled input
    - Again, I hope this works. This is in preparation for a much longer restructuring,
      one that will adjust how our design system is built and maintained

---
+++ src/components/helpers/uncontrolled-input/uncontrolled-input.test.jsx
@@ -0,0 +1,91 @@
+import React, { createRef } from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import UncontrolledInput from './uncontrolled-input.jsx';
+
+describe('src/components/helpers/controlled-input/uncontrolled-input', () => {
+  const renderComponent = (props = {}) => render(<UncontrolledInput id="test-id" {...props} />);
+  const testId = 'controlled input';
+
+  afterEach(cleanup);
+
+  describe('className API', () => {
+    it('accepts a className prop', () => {
+      const { getByTestId } = renderComponent({ className: 'bar' });
+      expect(getByTestId(testId)).toHaveClass('bar');
+    });
+  });
+
+  describe('disabled API', () => {
+    it('can be enabled', () => {
+      const { getByTestId } = renderComponent({ disabled: false });
+      expect(getByTestId(testId)).not.toBeDisabled();
+    });
+
+    it('can be disabled', () => {
+      const { getByTestId } = renderComponent({ disabled: true });
+      expect(getByTestId(testId)).toBeDisabled();
+    });
+  });
+
+  describe('id API', () => {
+    it('sets the ID attribute', () => {
+      const { getByTestId } = renderComponent();
+      expect(getByTestId(testId)).toHaveAttribute('id', 'test-id');
+    });
+  });
+
+  describe('inputRef API', () => {
+    it('sets the ref on itself', () => {
+      const inputRef = createRef();
+      const { getByTestId } = renderComponent({ inputRef });
+      expect(getByTestId(testId)).toBe(inputRef.current);
+    });
+  });
+
+  describe('type API', () => {
+    const types = ['text', 'date', 'number'];
+
+    types.forEach((type) => {
+      it(`accepts '${type}' as a valid type`, () => {
+        const { getByTestId } = renderComponent({ type });
+        expect(getByTestId(testId)).toHaveAttribute('type', type);
+      });
+    });
+  });
+
+  describe('interaction API', () => {
+    it('accepts onBlur', () => {
+      const onBlur = jest.fn();
+      const { getByTestId } = renderComponent({ onBlur });
+      fireEvent.blur(getByTestId(testId));
+      expect(onBlur.mock.calls.length).toBe(1);
+    });
+
+    it('accepts onChange', () => {
+      const onChange = jest.fn();
+      const { getByTestId } = renderComponent({ onChange });
+      fireEvent.change(getByTestId(testId), { target: { value: 'hello' } });
+      expect(onChange.mock.calls.length).toBe(1);
+    });
+
+    it('accepts onFocus', () => {
+      const onFocus = jest.fn();
+      const { getByTestId } = renderComponent({ onFocus });
+      fireEvent.focus(getByTestId(testId));
+      expect(onFocus.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('value API', () => {
+    it('accepts a starting value', () => {
+      const { getByTestId } = renderComponent({ value: 'hello' });
+      expect(getByTestId(testId)).toHaveValue('hello');
+    });
+
+    it('changes with an onChange event', () => {
+      const { getByTestId } = renderComponent({ value: 'hello' });
+      fireEvent.change(getByTestId(testId), { target: { value: 'goodbye' } });
+      expect(getByTestId(testId)).toHaveValue('goodbye');
+    });
+  });
+});
