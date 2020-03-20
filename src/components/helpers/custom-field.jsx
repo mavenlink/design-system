693040620a941b69c7f0e90db0079d3eacc90374
Andre LaFleur <alafleur@mavenlink.com>
2020-03-20 09:00:04 -0600

    Add ID and label

--- src/components/helpers/custom-field.jsx
+++ src/components/helpers/custom-field.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function CustomField() {
-  return (<div />);
+export default function CustomField(props) {
+  return (<div>
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+    </div>
+    <div>
+      <input id={props.id} />
+    </div>
+  </div>);
 }
+
+CustomField.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+CustomField.defaultProps = {};
--- src/components/helpers/custom-field.test.jsx
+++ src/components/helpers/custom-field.test.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { cleanup, render } from '@testing-library/react';
 import CustomField from './custom-field.jsx';
 
 describe('src/components/helpers/custom-field', () => {
+  const renderComponent = (props = {}) => render(<CustomField
+    id="custom-field"
+    label="Custom Field"
+    {...props}
+  />);
+
+  afterEach(cleanup);
+
   it('has defaults', () => {
-    expect(renderer.create(<CustomField />).toJSON()).toMatchSnapshot();
+    expect(renderer.create(<CustomField id="custom-field" label="Custom Field" />).toJSON()).toMatchSnapshot();
+  });
+
+  describe('label API', () => {
+    it('sets the label', () => {
+      const { getByLabelText } = renderComponent();
+      expect(getByLabelText('Custom Field')).toBeDefined();
+    });
   });
 });
