bdddca8ce179e15a3d134fe65794519437d5d24f
Andre LaFleur <alafleur@mavenlink.com>
2020-05-13 16:48:41 -0600

    Setup baseline for color linter
    - This is just the skeleton for a linter plugin to work, and for
      a test to ensure that it operates correctly
    
    Co-authored-by: Juan-Carlos Medina <juanca@mavenlink.com>

--- src/linters/colors.js
+++ src/linters/colors.js
@@ -1,3 +1,20 @@
-export default {
-  foo: 'bar',
-};
+import stylelint from 'stylelint';
+
+const ruleName = 'mds/colors';
+const url = 'https://mavenlink.github.io/design-system/master/#/Brand%20Identity?id=colors';
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  rejected: `Please use CSS variables included in MDS Color palette: ${url}`,
+});
+
+module.exports = stylelint.createPlugin(ruleName, (primaryOption, secondaryOption) => {
+  return (root, result) => {
+    stylelint.utils.report({
+      ruleName,
+      node: root,
+      message: messages.rejected,
+      result,
+    });
+  };
+});
+
+module.exports.ruleName = ruleName;
--- src/linters/colors.test.js
+++ src/linters/colors.test.js
@@ -1,7 +1,24 @@
+import stylelint from 'stylelint';
 import colors from './colors.js';
 
+const configuration = (options) => {
+  return Object.assign({
+    config: {
+      plugins: ['./colors.js'],
+      rules: {
+        [colors.ruleName]: [true, {}],
+      },
+    },
+    configBasedir: 'src/linters',
+  }, options);
+};
+
 describe('src/linters/colors', () => {
-  it('does a thing', () => {
-    expect(colors.foo).toBe('bar');
+  it('does a thing', async () => {
+    const code = '.should-fail { color: #ffaabb; }';
+
+    await stylelint.lint(configuration({ code })).then((result) => {
+      expect(result.errored).toBe(true);
+    });
   });
 });
