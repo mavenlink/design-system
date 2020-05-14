d20b8c0d0c7ad7226bab4dc7e0de6059b899715d
Juan-Carlos Medina <juanca@mavenlink.com>
2020-05-14 09:02:38 -0700

    Implement test cases for all kinds of color values
    Most of these are sourced from https://developer.mozilla.org/en-US/docs/Web/CSS/color_value

--- src/linters/colors.test.js
+++ src/linters/colors.test.js
@@ -14,8 +14,72 @@ const configuration = (options) => {
 };
 
 describe('src/linters/colors', () => {
-  it('fails on hex values', async () => {
+  it('fails on named colors values', async () => {
+    const code = '.should-fail { color: aliceblue; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on 3-digit hex values', async () => {
+    const code = '.should-fail { color: #ffa; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on 6-digit hex values', async () => {
     const code = '.should-fail { color: #ffaabb; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on 4-digit hex values', async () => {
+    const code = '.should-fail { color: #ffaa; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on 8-digit hex values', async () => {
+    const code = '.should-fail { color: #ffaaffaa; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on rgb values', async () => {
+    const code = '.should-fail { color: rgb(0, 0, 0); }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on rgba values', async () => {
+    const code = '.should-fail { color: rgba(0, 0, 0, 0); }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on hsl values', async () => {
+    const code = '.should-fail { color: hsl(0, 0, 0); }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fails on hsla values', async () => {
+    const code = '.should-fail { color: hsla(0, 0, 0, 0); }';
 
     await stylelint.lint(configuration({ code })).then((data) => {
       expect(data.errored).toBe(true);
