26f93f74dd5bcdae9e804953cbfedef5bb4ca1ab
Andre LaFleur <alafleur@mavenlink.com>
2020-05-14 11:16:23 -0600

    Handle CSS colors case
    - List of CSS colors were acquired here:
      https://raw.githubusercontent.com/bahamas10/css-color-names/master/css-color-names.json

--- src/linters/colors.js
+++ src/linters/colors.js
@@ -1,4 +1,5 @@
 import stylelint from 'stylelint';
+import cssColorMap from './data/css-colors.json';
 
 const ruleName = 'mds/colors';
 const url = 'https://mavenlink.github.io/design-system/master/#/Brand%20Identity?id=colors';
@@ -6,23 +7,29 @@ const solution = `Please use MDS variables instead. See ${url}`;
 
 const messages = stylelint.utils.ruleMessages(ruleName, {
   noHex: `Avoid using hex codes. ${solution}`,
+  noCssColors: `Avoid using CSS colors. ${solution}`,
 });
 
-const properties = ['color'];
+const properties = ['color', 'background-color'];
 const hasHex = value => value.match(/#[a-fA-F0-9]+/);
+const isCssColor = value => Object.keys(cssColorMap).includes(value);
 
 module.exports = stylelint.createPlugin(ruleName, (primaryOption, secondaryOption) => {
   return (root, result) => {
     root.walkDecls((declaration) => {
       const property = declaration.prop;
+      const invalidColor = hasHex(declaration.value) || isCssColor(declaration.value);
 
-      if (properties.includes(property) && hasHex(declaration.value)) {
-        stylelint.utils.report({
-          ruleName,
-          node: root,
-          message: messages.noHex,
-          result,
-        });
+      if (properties.includes(property) && invalidColor) {
+        const violation = { ruleName, node: declaration, result };
+
+        if (hasHex(declaration.value)) {
+          stylelint.utils.report({ ...violation, message: messages.noHex });
+        }
+
+        if (isCssColor(declaration.value)) {
+          stylelint.utils.report({ ...violation, message: messages.noCssColors });
+        }
       }
     });
   };
--- src/linters/colors.test.js
+++ src/linters/colors.test.js
@@ -25,4 +25,16 @@ describe('src/linters/colors', () => {
       expect(message).toContain('Avoid using hex codes. Please use MDS variables instead. See https://mavenlink.github.io/design-system/master/#/Brand%20Identity?id=colors');
     });
   });
+
+  it('fails with CSS colors', async () => {
+    const code = '.should-fail { background-color: aliceblue; }';
+
+    await stylelint.lint(configuration({ code })).then((result) => {
+      expect(result.errored).toBe(true);
+      expect(result.results.length).toBe(1);
+
+      const message = result.results[0].warnings[0].text;
+      expect(message).toContain('Avoid using CSS colors. Please use MDS variables instead. See https://mavenlink.github.io/design-system/master/#/Brand%20Identity?id=colors');
+    });
+  });
 });
