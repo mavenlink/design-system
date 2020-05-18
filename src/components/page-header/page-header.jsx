2bf1d63932dd06c588e8c9c0f7dcb1b10f47f48d
Juan-Carlos Medina <juanca@mavenlink.com>
2020-05-18 13:26:38 -0700

    Implement spacing rules for PageHeader

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './page-header.css';
 
 export default function PageHeader(props) {
   return (
-    <header>
-      <h1>{props.title}</h1>
-      {props.description && <p>{props.description}</p>}
+    <header className={styles.container}>
+      <h1 className={styles.heading}>{props.title}</h1>
+      {props.description && (
+        <p className={styles.description}>{props.description}</p>
+      )}
     </header>
   );
 }
