037e5c9b67914eb9f17c321f1465c8d37ef9b821
Juan-Carlos Medina <juanca@mavenlink.com>
2020-05-28 12:24:47 -0700

    Implement TagSkill as composition from Tag
    Co-authored-by: Greyson Richey <grichey@mavenlink.com>

--- src/components/tag-skill/tag-skill.jsx
+++ src/components/tag-skill/tag-skill.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import iconTick from '../../svgs/icon-tick.svg';
+import Icon from '../icon/icon.jsx';
+import Tag from '../tag/tag.jsx';
+import styles from './tag-skill.css';
 
 export default function TagSkill(props) {
   return (
-    <React.Fragment>
-      <span>Skillet</span>
-      <span> - </span>
-      <span>{props.name}</span>
-      <span> - </span>
-      <span>{props.level}</span>
-    </React.Fragment>
+    <Tag>
+      <span className={styles.row}>
+        {props.name}
+        <span className={styles.level}>
+          {props.level ? (
+            props.level
+          ) : (
+            <Icon name={iconTick.id} size="small" stroke="skip" fill="skip" currentColor="skip" />
+          )}
+        </span>
+      </span>
+    </Tag>
   );
 }
 
