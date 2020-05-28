01324586027e8c92ca4ed28430574b5cb018b943
Juan-Carlos Medina <juanca@mavenlink.com>
2020-05-28 11:08:36 -0700

    Lint fix: add semi-colon

--- src/components/tag/tag.test.jsx
+++ src/components/tag/tag.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import Tag from './tag.jsx';
 
 describe('Tag', () => {
