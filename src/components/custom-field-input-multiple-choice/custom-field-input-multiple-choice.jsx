540fc9b25750e933bf60c7d2bcd29a51d032f5f5
Juan-Carlos Medina <juanca@mavenlink.com>
2020-06-09 17:19:30 -0700

    Add the caret down icons on CustomFieldInputMultipleChoice
    Moving margin to the right of each tag since the container can do its own padding-left to shift within a container

--- src/components/custom-field-input-multiple-choice/custom-field-input-multiple-choice.jsx
+++ src/components/custom-field-input-multiple-choice/custom-field-input-multiple-choice.jsx
@@ -4,6 +4,9 @@ import React, {
   useImperativeHandle,
 } from 'react';
 import PropTypes from 'prop-types';
+import Icon from '../icon/icon.jsx';
+import iconCaretDown from '../../svgs/icon-caret-down.svg';
+import iconCaretDownDisabled from '../../svgs/icon-caret-down-disabled.svg';
 import TagList from '../tag-list/tag-list.jsx';
 import Tag from '../tag/tag.jsx';
 import styles from './custom-field-input-multiple-choice.css';
@@ -32,6 +35,7 @@ const CustomFieldInputMultipleChoice = forwardRef(function CustomFieldInputMulti
           {choice.label}
         </Tag>
       ))}
+      <Icon className={styles['input-icon']} name={props.readOnly ? iconCaretDownDisabled.id : iconCaretDown.id} fill="skip" />
     </TagList>
   );
 });
