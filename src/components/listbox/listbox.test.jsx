0daa0b9781003b39545188ddad566b8faf6e94e3
Juan-Carlos Medina <juanca@mavenlink.com>
2020-06-17 13:27:43 -0700

    Add test to ensure Listbox can have proper page tab sequence indices

--- src/components/listbox/listbox.test.jsx
+++ src/components/listbox/listbox.test.jsx
@@ -54,5 +54,28 @@ describe('src/components/listbox/listbox', () => {
       expect(screen.getByText('Hello')).not.toHaveFocus();
       expect(screen.getByText('Hey')).not.toHaveFocus();
     });
+
+    it('only has the one option in the page tab sequence', async () => {
+      // TODO(@testing-library/user-event) Update user-event lib when https://github.com/testing-library/user-event/issues/365 is merged
+      render((
+        <Listbox {...requiredProps}>
+          <ListOption key="yeah" defaultActive={true}>Hello</ListOption>
+          <ListOption key="yeah2" defaultActive={false}>Hey</ListOption>
+          <button />
+        </Listbox>
+      ));
+
+      expect(document.body).toHaveFocus();
+
+      userEvent.tab();
+      expect(screen.getByText('Hello')).toHaveFocus();
+
+      userEvent.tab();
+      expect(screen.getByText('Hello')).not.toHaveFocus();
+      expect(screen.getByText('Hey')).not.toHaveFocus();
+
+      userEvent.tab({ shift: true });
+      expect(screen.getByText('Hello')).toHaveFocus();
+    });
   });
 });
