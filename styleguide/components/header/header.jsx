543eaeb0613435d5d7af36cb49e5a9e4b1a584c8
Juan-Carlos Medina <juanca@mavenlink.com>
2020-06-23 14:32:19 -0700

    Update MDS site header to use latest typography and color tokens
    In addition, avoid hard-coding heights. Use better `sticky` positioning

--- styleguide/components/header/header.jsx
+++ styleguide/components/header/header.jsx
@@ -5,15 +5,19 @@ import styles from './header.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.section}>
-        <Logo />
-        <span className={styles.title}>
-          Design System
-          <span className={styles.end}> / Front-end</span>
-        </span>
-      </div>
-      <div className={styles.section}>
-        <a className={styles.github} href="https://github.com/mavenlink/design-system">Github</a>
+      <div className={styles['header-content']}>
+        <div className={styles.section}>
+          <Logo />
+          <span className={styles.title}>
+            Design System
+          </span>
+          <span className={styles.title2}>
+            &nbsp;/ Front-end
+          </span>
+        </div>
+        <div className={styles.section}>
+          <a className={styles.github} href="https://github.com/mavenlink/design-system">Github</a>
+        </div>
       </div>
     </header>
   );
