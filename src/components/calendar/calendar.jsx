46c95b2b73545287b2a106be83bdebf722a8949b
Juan-Carlos Medina <juanca@mavenlink.com>
2020-07-29 16:51:10 -0700

    Implement previous and next month buttons on Calendar

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {
+  useState,
+} from 'react';
 import PropTypes from 'prop-types';
 
 function Calendar(props) {
-  const date = new Date(props.value ? props.value : Date.now());
+  const valueDate = new Date(props.value ? props.value : Date.now());
+  const [year] = useState(valueDate.getFullYear());
+  const [month, setMonth] = useState(valueDate.getMonth());
+  const [date] = useState(valueDate.getDate());
+
+  function onPreviousMonthPress() {
+    setMonth(month - 1);
+  }
+
+  function onNextMonthPress() {
+    setMonth(month + 1);
+  }
+
+  const calendarDate = new Date(year, month, date);
 
   return (
     <div>
-      {date.toLocaleDateString(undefined, {
+      <button onClick={onPreviousMonthPress}>Prev</button>
+      {calendarDate.toLocaleDateString(undefined, {
         year: 'numeric',
         month: 'long',
       })}
+      <button onClick={onNextMonthPress}>Next</button>
     </div>
   );
 }
--- src/components/calendar/calendar.test.jsx
+++ src/components/calendar/calendar.test.jsx
@@ -3,6 +3,7 @@ import {
   render,
   screen,
 } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Calendar from './calendar.jsx';
 
 describe('<Calendar />', () => {
@@ -12,6 +13,24 @@ describe('<Calendar />', () => {
   it('has defaults', () => {
     jest.spyOn(Date, 'now').mockImplementation(() => 1596062788778) // July 29, 2020
     expect(render(<Calendar {...requiredProps} />)).toMatchSnapshot();
+  });
+
+  describe('previous month button', () => {
+    it('displays the previous month', () => {
+      jest.spyOn(Date, 'now').mockImplementation(() => 1596062788778) // July 29, 2020
+      render(<Calendar {...requiredProps} />);
+      userEvent.click(screen.getByText('Prev'));
+      expect(screen.getByText('June 2020')).toBeInTheDocument();
+    });
+  });
+
+  describe('next month button', () => {
+    it('displays the next month', () => {
+      jest.spyOn(Date, 'now').mockImplementation(() => 1596062788778) // July 29, 2020
+      render(<Calendar {...requiredProps} />);
+      userEvent.click(screen.getByText('Next'));
+      expect(screen.getByText('August 2020')).toBeInTheDocument();
+    });
   });
 
   describe('value props API', () => {
