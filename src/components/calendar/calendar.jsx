7d23ebece6b8aa1dd0f25605a73c671c9ac2dda5
Juan-Carlos Medina <juanca@mavenlink.com>
2020-08-04 12:08:48 -0700

    Fix linter violations: comment out future code for handling active date, add ;
    Co-authored-by: Ben Nappier <ben@mavenlink.com>

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -60,7 +60,7 @@ function Calendar(props) {
   const highlightedDate = new Date(props.value ? `${props.value}T00:00` : Date.now());
   const [year, setYear] = useState(highlightedDate.getFullYear());
   const [month, setMonth] = useState(highlightedDate.getMonth());
-  const [date] = useState(highlightedDate.getDate());
+  // const [date] = useState(highlightedDate.getDate());
 
   function onPreviousMonthPress() {
     const tmpDate = new Date(year, month - 1);
