32b1cd49af567be0a2e4e07cff522c930ad5061e
Ben Nappier <ben@mavenlink.com>
2020-08-07 12:53:23 -0600

    Stub timezone to avoid snapshot timezone difference testing issues;
    - Add coverage for keyboard nav

--- src/components/calendar/calendar.test.jsx
+++ src/components/calendar/calendar.test.jsx
@@ -13,7 +13,17 @@ describe('<Calendar />', () => {
 
   beforeEach(() => {
     jest.spyOn(Date, 'now').mockImplementation(() => 1596062788778); // July 29, 2020
+    jest.spyOn(Date.prototype, 'getTimezoneOffset').mockImplementation(() => 360); // Force timezone to MDT
   });
+
+  function renderAndExpectFocusedCalendar() {
+    render(<Calendar {...requiredProps} />);
+    userEvent.tab();
+    userEvent.tab();
+    userEvent.tab();
+    userEvent.tab();
+    expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveFocus();
+  }
 
   it('has defaults', () => {
     render(<Calendar {...requiredProps} />);
@@ -59,67 +69,75 @@ describe('<Calendar />', () => {
 
     describe('when a date is focused', () => {
       it('selects the active date on Enter', () => {
-        render(<Calendar {...requiredProps} />);
-        userEvent.tab();
-        userEvent.tab();
-        userEvent.tab();
-        userEvent.tab();
-        expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveFocus();
+        renderAndExpectFocusedCalendar();
         expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'false');
         fireEvent.keyDown(screen.getByRole('gridcell', { name: 'July 29' }), { key: 'Enter' });
         expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'true');
       });
 
       it('selects the active date on Space', () => {
-        render(<Calendar {...requiredProps} />);
-        userEvent.tab();
-        userEvent.tab();
-        userEvent.tab();
-        userEvent.tab();
-        expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveFocus();
+        renderAndExpectFocusedCalendar();
         expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'false');
         fireEvent.keyDown(screen.getByRole('gridcell', { name: 'July 29' }), { key: 'Space' });
         expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'true');
       });
 
       it('moves the focus to the previous date on LeftArrow', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowLeft', code: 'ArrowLeft' });
+        expect(screen.getByRole('gridcell', { name: 'July 28' })).toHaveFocus();
       });
 
       it('moves the focus to the next date on RightArrow', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowRight', code: 'ArrowRight' });
+        expect(screen.getByRole('gridcell', { name: 'July 30' })).toHaveFocus();
       });
 
       it('moves the focus to the previous week on UpArrow', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowUp', code: 'ArrowUp' });
+        expect(screen.getByRole('gridcell', { name: 'July 22' })).toHaveFocus();
       });
 
       it('moves the focus to the next week on DownArrow', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowDown', code: 'ArrowDown' });
+        expect(screen.getByRole('gridcell', { name: 'August 5' })).toHaveFocus();
       });
 
       it('moves the focus to the beginning of the week on Home', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'Home', code: 'Home' });
+        expect(screen.getByRole('gridcell', { name: 'July 26' })).toHaveFocus();
       });
 
       it('moves the focus to the end of the week on End', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'End', code: 'End' });
+        expect(screen.getByRole('gridcell', { name: 'August 1' })).toHaveFocus();
       });
 
       it('changes the calendar to the previous month on PageUp', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'PageUp', code: 'PageUp' });
+        expect(screen.getByRole('gridcell', { name: 'June 29' })).toHaveFocus();
       });
 
       it('changes the calendar to the next month on PageDown', () => {
-        render(<Calendar {...requiredProps} />);
+        renderAndExpectFocusedCalendar();
+        fireEvent.keyDown(screen.getByRole('grid'), { key: 'PageDown', code: 'PageDown' });
+        expect(screen.getByRole('gridcell', { name: 'August 29' })).toHaveFocus();
       });
 
       it('changes the calendar to the previous year on Shift+PageUp', () => {
-        render(<Calendar {...requiredProps} />);
+        // renderAndExpectFocusedCalendar();
+        // fireEvent.keyDown(screen.getByRole('grid'), { key: 'PageDown', code: 'PageDown', meta: '{shift}' });
+        // expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveFocus();
       });
 
       it('changes the calendar to the next year on Shift+PageDown', () => {
-        render(<Calendar {...requiredProps} />);
+        // render(<Calendar {...requiredProps} />);
       });
     });
 
