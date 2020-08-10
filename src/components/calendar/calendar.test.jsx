b8abf80e0c121e390560113d2fab3a563b1fc60f
Ben Nappier <ben@mavenlink.com>
2020-08-10 13:26:44 -0600

    Avoid selectors for getBy* helpers to improve calendar test performance

--- src/components/calendar/calendar.test.jsx
+++ src/components/calendar/calendar.test.jsx
@@ -21,7 +21,7 @@ describe('<Calendar />', () => {
     userEvent.tab();
     userEvent.tab();
     userEvent.tab();
-    expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveFocus();
+    expect(screen.getByLabelText('July 29')).toHaveFocus();
   }
 
   it('has defaults', () => {
@@ -31,32 +31,32 @@ describe('<Calendar />', () => {
 
   it('renders the previous month', () => {
     render(<Calendar {...requiredProps} />);
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to June 2020' }));
-    expect(screen.getByRole('gridcell', { name: 'May 31' })).toHaveClass('not-current-date');
-    expect(screen.getByRole('gridcell', { name: 'June 1' })).toHaveClass('date');
-    expect(screen.getByRole('gridcell', { name: 'July 1' })).toHaveClass('not-current-date');
-    expect(screen.getByRole('gridcell', { name: 'July 11' })).toHaveClass('not-current-date');
+    userEvent.click(screen.getByTitle('Change calendar to June 2020'));
+    expect(screen.getByLabelText('May 31')).toHaveClass('not-current-date');
+    expect(screen.getByLabelText('June 1')).toHaveClass('date');
+    expect(screen.getByLabelText('July 1')).toHaveClass('not-current-date');
+    expect(screen.getByLabelText('July 11')).toHaveClass('not-current-date');
     expect(document.body).toMatchSnapshot();
   });
 
   it('renders the previous year', () => {
     render(<Calendar {...requiredProps} />);
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to June 2020' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to May 2020' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to April 2020' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to March 2020' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to February 2020' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to January 2020' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to December 2019' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to November 2019' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to October 2019' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to September 2019' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to August 2019' }));
-    userEvent.click(screen.getByRole('button', { name: 'Change calendar to July 2019' }));
-    expect(screen.getByRole('gridcell', { name: 'June 30' })).toHaveClass('not-current-date');
-    expect(screen.getByRole('gridcell', { name: 'July 30' })).toHaveClass('date');
-    expect(screen.getByRole('gridcell', { name: 'July 1' })).toHaveClass('date');
-    expect(screen.getByRole('gridcell', { name: 'August 1' })).toHaveClass('not-current-date');
+    userEvent.click(screen.getByTitle('Change calendar to June 2020'));
+    userEvent.click(screen.getByTitle('Change calendar to May 2020'));
+    userEvent.click(screen.getByTitle('Change calendar to April 2020'));
+    userEvent.click(screen.getByTitle('Change calendar to March 2020'));
+    userEvent.click(screen.getByTitle('Change calendar to February 2020'));
+    userEvent.click(screen.getByTitle('Change calendar to January 2020'));
+    userEvent.click(screen.getByTitle('Change calendar to December 2019'));
+    userEvent.click(screen.getByTitle('Change calendar to November 2019'));
+    userEvent.click(screen.getByTitle('Change calendar to October 2019'));
+    userEvent.click(screen.getByTitle('Change calendar to September 2019'));
+    userEvent.click(screen.getByTitle('Change calendar to August 2019'));
+    userEvent.click(screen.getByTitle('Change calendar to July 2019'));
+    expect(screen.getByLabelText('June 30')).toHaveClass('not-current-date');
+    expect(screen.getByLabelText('July 30')).toHaveClass('date');
+    expect(screen.getByLabelText('July 1')).toHaveClass('date');
+    expect(screen.getByLabelText('August 1')).toHaveClass('not-current-date');
     expect(document.body).toMatchSnapshot();
   });
 
@@ -69,64 +69,64 @@ describe('<Calendar />', () => {
     describe('when a date is focused', () => {
       it('selects the active date on Enter', () => {
         renderAndExpectFocusedCalendar();
-        expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'false');
-        fireEvent.keyDown(screen.getByRole('gridcell', { name: 'July 29' }), { key: 'Enter' });
-        expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'true');
+        expect(screen.getByLabelText('July 29')).toHaveAttribute('aria-selected', 'false');
+        fireEvent.keyDown(screen.getByLabelText('July 29'), { key: 'Enter' });
+        expect(screen.getByLabelText('July 29')).toHaveAttribute('aria-selected', 'true');
       });
 
       it('selects the active date on Space', () => {
         renderAndExpectFocusedCalendar();
-        expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'false');
-        fireEvent.keyDown(screen.getByRole('gridcell', { name: 'July 29' }), { key: 'Space' });
-        expect(screen.getByRole('gridcell', { name: 'July 29' })).toHaveAttribute('aria-selected', 'true');
+        expect(screen.getByLabelText('July 29')).toHaveAttribute('aria-selected', 'false');
+        fireEvent.keyDown(screen.getByLabelText('July 29'), { key: 'Space' });
+        expect(screen.getByLabelText('July 29')).toHaveAttribute('aria-selected', 'true');
       });
 
       it('moves the focus to the previous date on LeftArrow', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowLeft', code: 'ArrowLeft' });
-        expect(screen.getByRole('gridcell', { name: 'July 28' })).toHaveFocus();
+        expect(screen.getByLabelText('July 28')).toHaveFocus();
       });
 
       it('moves the focus to the next date on RightArrow', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowRight', code: 'ArrowRight' });
-        expect(screen.getByRole('gridcell', { name: 'July 30' })).toHaveFocus();
+        expect(screen.getByLabelText('July 30')).toHaveFocus();
       });
 
       it('moves the focus to the previous week on UpArrow', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowUp', code: 'ArrowUp' });
-        expect(screen.getByRole('gridcell', { name: 'July 22' })).toHaveFocus();
+        expect(screen.getByLabelText('July 22')).toHaveFocus();
       });
 
       it('moves the focus to the next week on DownArrow', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'ArrowDown', code: 'ArrowDown' });
-        expect(screen.getByRole('gridcell', { name: 'August 5' })).toHaveFocus();
+        expect(screen.getByLabelText('August 5')).toHaveFocus();
       });
 
       it('moves the focus to the beginning of the week on Home', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'Home', code: 'Home' });
-        expect(screen.getByRole('gridcell', { name: 'July 26' })).toHaveFocus();
+        expect(screen.getByLabelText('July 26')).toHaveFocus();
       });
 
       it('moves the focus to the end of the week on End', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'End', code: 'End' });
-        expect(screen.getByRole('gridcell', { name: 'August 1' })).toHaveFocus();
+        expect(screen.getByLabelText('August 1')).toHaveFocus();
       });
 
       it('changes the calendar to the previous month on PageUp', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'PageUp', code: 'PageUp' });
-        expect(screen.getByRole('gridcell', { name: 'June 29' })).toHaveFocus();
+        expect(screen.getByLabelText('June 29')).toHaveFocus();
       });
 
       it('changes the calendar to the next month on PageDown', () => {
         renderAndExpectFocusedCalendar();
         fireEvent.keyDown(screen.getByRole('grid'), { key: 'PageDown', code: 'PageDown' });
-        expect(screen.getByRole('gridcell', { name: 'August 29' })).toHaveFocus();
+        expect(screen.getByLabelText('August 29')).toHaveFocus();
       });
 
       xit('changes the calendar to the previous year on Shift+PageUp', () => {
@@ -148,16 +148,16 @@ describe('<Calendar />', () => {
 
     it('selects a date on click', () => {
       render(<Calendar {...requiredProps} />);
-      expect(screen.getByRole('gridcell', { name: 'July 28' })).toHaveAttribute('aria-selected', 'false');
-      userEvent.click(screen.getByRole('gridcell', { name: 'July 28' }));
-      expect(screen.getByRole('gridcell', { name: 'July 28' })).toHaveAttribute('aria-selected', 'true');
+      expect(screen.getByLabelText('July 28')).toHaveAttribute('aria-selected', 'false');
+      userEvent.click(screen.getByLabelText('July 28'));
+      expect(screen.getByLabelText('July 28')).toHaveAttribute('aria-selected', 'true');
     });
   });
 
   describe('previous month button', () => {
     it('displays the previous month', () => {
       render(<Calendar {...requiredProps} />);
-      userEvent.click(screen.getByRole('button', { name: 'Change calendar to June 2020' }));
+      userEvent.click(screen.getByTitle('Change calendar to June 2020'));
       expect(screen.getByText('June 2020')).toBeInTheDocument();
     });
   });
@@ -165,7 +165,7 @@ describe('<Calendar />', () => {
   describe('next month button', () => {
     it('displays the next month', () => {
       render(<Calendar {...requiredProps} />);
-      userEvent.click(screen.getByRole('button', { name: 'Change calendar to August 2020' }));
+      userEvent.click(screen.getByTitle('Change calendar to August 2020'));
       expect(screen.getByText('August 2020')).toBeInTheDocument();
     });
   });
@@ -174,7 +174,7 @@ describe('<Calendar />', () => {
     it('can be set', () => {
       expect(render(<Calendar {...requiredProps} value="2012-02-28" />));
       expect(screen.getByText('February 2012')).toBeInTheDocument();
-      expect(screen.getByRole('gridcell', { name: 'February 28' })).toHaveAttribute('aria-selected', 'true');
+      expect(screen.getByLabelText('February 28')).toHaveAttribute('aria-selected', 'true');
     });
   });
 });
