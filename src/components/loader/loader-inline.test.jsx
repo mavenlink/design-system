645fe1c15e3285564a023dc7ef45b904ac31f025
Andre LaFleur <alafleur@mavenlink.com>
2020-08-19 11:51:59 -0600

    backfill specs on inline loader

--- src/components/loader/loader-inline.test.jsx
+++ src/components/loader/loader-inline.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import LoaderInline from './loader-inline.jsx';
 
@@ -6,5 +7,17 @@ describe('LoaderInline', () => {
   it('has defaults', () => {
     const tree = renderer.create((<LoaderInline />)).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('styling props', () => {
+    it('permits provided inline styling', () => {
+      render(<LoaderInline cssInline="goof" />);
+      expect(screen.getByText('Loading...')).toHaveClass('goof');
+    });
+
+    it('permits provided inline container styling', () => {
+      render(<LoaderInline cssInlineWrapper="gaff" />);
+      expect(screen.getByTestId('loader')).toHaveClass('gaff');
+    });
   });
 });
