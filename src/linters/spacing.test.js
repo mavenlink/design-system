50e6ac28858cd7d30ae63055e97d316cda863823
Ben Nappier <ben@mavenlink.com>
2020-08-21 13:07:45 -0600

    Improve coverage on spacing linter tests

--- src/linters/spacing.test.js
+++ src/linters/spacing.test.js
@@ -1,7 +1,7 @@
 import stylelint from 'stylelint';
 import spacing from './spacing.js';
 
-const configuration = (options) => {
+const configuration = (code, options) => {
   return Object.assign({
     config: {
       plugins: ['./spacing.js'],
@@ -10,15 +10,57 @@ const configuration = (options) => {
       },
     },
     configBasedir: 'src/linters',
-  }, options);
+    fix: options && options.fix,
+  }, code);
 };
 
 describe('src/linters/spacing', () => {
-  it('do', async () => {
+  it('errors on a mds variable value', async () => {
     const code = '.className { margin: 2px; }';
 
     await stylelint.lint(configuration({ code })).then((data) => {
       expect(data.errored).toBe(true);
+    });
+  });
+
+  it('does not errors on a value differnet from a mds variable value', async () => {
+    const code = '.className { margin: 3px; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(false);
+    });
+  });
+
+  it('does not error on an attribute other than margin or padding', async () => {
+    const code = '.className { border: 2px; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(false);
+    });
+  });
+
+  it('errors on margin', async () => {
+    const code = '.className { margin: 16px; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('errors on padding', async () => {
+    const code = '.className { padding: 32px; }';
+
+    await stylelint.lint(configuration({ code })).then((data) => {
+      expect(data.errored).toBe(true);
+    });
+  });
+
+  it('fixes mds variables to their value when fixed is passed in and does not error', async () => {
+    const code = '.className { margin: 16px; }';
+
+    await stylelint.lint(configuration({ code }, { fix: true })).then((data) => {
+      expect(data.errored).toBe(false);
+      expect(data.output).toEqual('.className { margin: var(--spacing-large); }');
     });
   });
 });
