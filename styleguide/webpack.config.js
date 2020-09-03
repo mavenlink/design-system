8f179df38ebc6aafc4347cb35461fc598ee2b8f1
Juan-Carlos Medina <juanca@mavenlink.com>
2020-09-03 13:39:55 -0700

    chore(webpack.config): simplify webpack config around postcss configurations

--- styleguide/webpack.config.js
+++ styleguide/webpack.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-commonjs */
 
-const path = require('path');
 const postCssPresetEnv = require('postcss-preset-env');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
@@ -48,27 +47,8 @@ module.exports = {
           {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',
               plugins: () => [
-                postCssPresetEnv({
-                  stage: false,
-                  features: {
-                    'custom-properties': {
-                      // TODO: Figure out how to get rid of the `importFrom` option. It's needed because:
-                      //
-                      // 1. postcss-loader runs before css-loader.
-                      // 2. css-loader therefore hasn't processed the CSS imports, yet.
-                      // 3. postcss-loader doesn't know about imported files when backfilling fallbacks for css variables.
-                      //
-                      // I've investigated resolving this by using postcss-import to process CSS imports. It works, but ends
-                      // duplicating imported files. See c36ce551090fb25f455821a32219a8081a8c4d67.
-                      importFrom: [
-                        path.join(__dirname, '../src/styles/colors.css'),
-                        path.join(__dirname, '../src/styles/typography.css'),
-                      ],
-                    },
-                  },
-                }),
+                postCssPresetEnv(),
               ],
             },
           },
