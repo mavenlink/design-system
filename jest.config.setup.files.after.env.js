2cea52ed18f00c6e00dbcbaad78979e0eb375a17
Ben Nappier <ben@mavenlink.com>
2020-09-09 15:59:16 -0600

    Fix comment order to make it easier to comment and uncomment

--- jest.config.setup.files.after.env.js
+++ jest.config.setup.files.after.env.js
@@ -1,5 +1,6 @@
+//   During test development, it might be useful to comment out this error handling. It is useful for ensuring our tests
+//   do not violate propTypes but it can make stack traces much harder to understand.
+
 console.error = (message) => { // eslint-disable-line
-  // During test development, it might be useful to comment out this error handling. It is useful for ensuring our tests
-  // do not violate propTypes but it can make stack traces much harder to understand.
   throw new Error(message);
 };
