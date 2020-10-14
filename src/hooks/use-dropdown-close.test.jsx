8f85e7239a08723ed44b1ddf22cc4a9755f06301
Juan-Carlos Medina <juanca@mavenlink.com>
2020-10-14 10:21:15 -0700

    fix(useDropdownClose): always bind `onClose` callback in case it has a closure on the render callstack
    For example, if a component were to set some state with `onClose` then it used to set state as if it was rendered the first time.

---
+++ src/hooks/use-dropdown-close.test.jsx
@@ -0,0 +1,65 @@
+import React, {
+  useRef,
+  useState,
+} from 'react';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import useDropdownClose from './use-dropdown-close.js';
+
+describe('useDropdownClose', () => {
+  function TestComponent() {
+    const ref = useRef();
+    const [opened, setOpened] = useState(false);
+
+    function onClose() {
+      setOpened(false);
+    }
+
+    function onFocus() {
+      setOpened(true);
+    }
+
+    useDropdownClose(ref, opened, onClose)
+
+    return (
+      <div onFocus={onFocus} ref={ref} tabIndex="0">{opened ? 'opened' : 'closed'}</div>
+    );
+  }
+
+  it('can be used in a component', () => {
+    render(<TestComponent />);
+    expect(document.body).toMatchSnapshot();
+  });
+
+  it('stays opens', () => {
+    render(<TestComponent />);
+    userEvent.tab();
+    expect(screen.getByText('opened')).toBeInTheDocument();
+    userEvent.click(screen.getByText('opened'));
+    expect(screen.getByText('opened')).toBeInTheDocument();
+  });
+
+  it('closes on blur', () => {
+    render((
+      <React.Fragment>
+        <TestComponent />
+        <input />
+      </React.Fragment>
+    ));
+    userEvent.tab();
+    expect(screen.getByText('opened')).toBeInTheDocument();
+    userEvent.tab();
+    expect(screen.getByText('closed')).toBeInTheDocument();
+  });
+
+  it('closes on click away', () => {
+    render(<TestComponent />);
+    userEvent.tab();
+    expect(screen.getByText('opened')).toBeInTheDocument();
+    userEvent.click(document.body);
+    expect(screen.getByText('closed')).toBeInTheDocument();
+  });
+});
