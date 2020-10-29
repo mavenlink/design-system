de4831784c6b29b4b816048972a9e664c360e32a
Andre LaFleur <alafleur@mavenlink.com>
2020-10-29 11:43:37 -0600

    Get required and validation working or something

--- src/components/number/number.jsx
+++ src/components/number/number.jsx
@@ -1,19 +1,42 @@
-import React, { useImperativeHandle } from 'react';
+import React, { useImperativeHandle, useRef } from 'react';
 import PropTypes from 'prop-types';
 import FormControl from '../form-control/form-control.jsx';
+import useValidation from '../../hooks/use-validation';
 
 const Number = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+  const [validationMessage, validate] = useValidation(props.validationMessage, inputRef);
+
+  function onBlur(event) {
+    validate();
+    props.onBlur(event);
+  }
+
+  function onChange(event) {
+    validate();
+    props.onChange(event);
+  }
+
   useImperativeHandle(ref, () => ({
     name: props.name,
   }));
 
   return (
-    <FormControl id={props.id} label={props.label}>
+    <FormControl
+      error={validationMessage}
+      id={props.id}
+      label={props.label}
+      required={props.required}
+    >
       <input
         aria-describedby={`${props.id}Hint`}
         id={props.id}
         placeholder={props.placeholder}
         name={props.name}
+        onBlur={onBlur}
+        onChange={onChange}
+        ref={inputRef}
+        required={props.required}
         type="number"
       />
     </FormControl>
@@ -24,12 +47,20 @@ Number.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   name: PropTypes.string,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
   placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  validationMessage: PropTypes.string,
 };
 
 Number.defaultProps = {
   name: undefined,
+  onBlur: () => {},
+  onChange: () => {},
   placeholder: undefined,
+  required: false,
+  validationMessage: '',
 };
 
 export default Number;
--- src/components/number/number.test.jsx
+++ src/components/number/number.test.jsx
@@ -5,6 +5,7 @@ import {
   cleanup,
 } from '@testing-library/react';
 import Number from './number.jsx';
+import userEvent from '@testing-library/user-event';
 
 describe('Number', () => {
   const requiredProps = {
@@ -45,6 +46,50 @@ describe('Number', () => {
     it('allows setting placeholder text', () => {
       render(<Number {...requiredProps} placeholder="Hello!" />);
       expect(screen.getByLabelText('Test Component')).toHaveAttribute('placeholder', 'Hello!');
+    });
+  });
+
+  describe('required API', () => {
+    it('can be set', () => {
+      render(<Number {...requiredProps} placeholder="Hello!" required />);
+      expect(screen.getByLabelText('Test Component')).toBeRequired();
+      expect(screen.getByLabelText('Test Component')).toBeInvalid();
+    });
+
+    it('can be unset', () => {
+      render(<Number {...requiredProps} required={false} />);
+      expect(screen.getByLabelText('Test Component')).not.toBeRequired();
+      expect(screen.getByLabelText('Test Component')).toBeValid();
+    });
+
+    it('is invalid on mount but does not have an error message', () => {
+      render(<Number {...requiredProps} required />);
+      expect(screen.getByLabelText('Test Component')).toBeInvalid();
+      expect(screen.getByLabelText('Test Component')).toHaveDescription('');
+    });
+
+    it('is invalid after typing', async () => {
+      render(<Number {...requiredProps} required={true} />);
+      userEvent.type(screen.getByLabelText('Test Component'), '2');
+      expect(screen.getByLabelText('Test Component')).toHaveValue(2);
+      expect(screen.getByLabelText('Test Component')).toBeValid();
+      expect(screen.getByLabelText('Test Component')).toHaveDescription('');
+      userEvent.type(screen.getByLabelText('Test Component'), '{backspace}');
+      expect(screen.getByLabelText('Test Component')).toHaveValue(null);
+      expect(screen.getByLabelText('Test Component')).toBeInvalid();
+      expect(screen.getByLabelText('Test Component')).toHaveDescription('Constraints not satisfied');
+    });
+
+    it('is invalid after tabbing through', async () => {
+      render(<Number {...requiredProps} required={true} />);
+      userEvent.tab();
+      expect(document.activeElement).toBe(screen.getByLabelText('Test Component'));
+      expect(screen.getByLabelText('Test Component')).toBeInvalid();
+      expect(screen.getByLabelText('Test Component')).toHaveDescription('');
+      userEvent.tab();
+      expect(document.activeElement).not.toBe(screen.getByLabelText('Test Component'));
+      expect(screen.getByLabelText('Test Component')).toBeInvalid();
+      expect(screen.getByLabelText('Test Component')).toHaveDescription('Constraints not satisfied');
     });
   });
 });
