1b692952ac1aceb920626b9e76b6651fe64685bc
Greyson Richey <grichey@mavenlink.com>
2021-02-02 16:04:32 -0700

    Remove container margin, add className API
    Co-authored-by: Juan-Carlos Medina <juanca@mavenlink.com>

--- src/components/page-header/page-header.test.jsx
+++ src/components/page-header/page-header.test.jsx
@@ -3,18 +3,27 @@ import { render, screen } from '@testing-library/react';
 import PageHeader from './page-header.jsx';
 
 describe('PageHeader', () => {
+  const requiredProps = {
+    title: 'Title',
+  };
+
   it('has defaults', () => {
-    render(<PageHeader title="Defaults" />);
+    render(<PageHeader {...requiredProps} />);
     expect(document.body).toMatchSnapshot();
   });
 
   it('has a title', () => {
-    render(<PageHeader title="Another title" />);
+    render(<PageHeader {...requiredProps} title="Another title" />);
     expect(screen.getByText('Another title').tagName).toEqual('H1');
   });
 
   it('has a description', () => {
-    render(<PageHeader title="Title" description="I am a description" />);
+    render(<PageHeader {...requiredProps} description="I am a description" />);
     expect(screen.getByText('I am a description').tagName).toEqual('P');
+  });
+
+  it('has a className API', () => {
+    render(<PageHeader {...requiredProps} className="test-class" />);
+    expect(screen.getByText('Title').parentElement).toHaveClass('test-class');
   });
 });
