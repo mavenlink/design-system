b2522129aa61f813ca6f3d7f2018c866ae755b3b
Greyson Richey <grichey@mavenlink.com>
2021-02-03 14:30:43 -0700

    Turn off import lint

--- src/mocks/jest-server.js
+++ src/mocks/jest-server.js
@@ -1,4 +1,4 @@
-import { setupServer } from 'msw/node';
+import { setupServer } from 'msw/node'; // eslint-disable-line import/extensions
 import handlers from './handlers.js';
 
 const jestServer = setupServer(...handlers);
