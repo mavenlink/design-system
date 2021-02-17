e14e27c2351525a57c20109e96fab02c81efa726
Greyson Richey <grichey@mavenlink.com>
2021-02-17 12:32:49 -0700

    Use localhost instead of a made-up remote endpoint

--- src/mocks/mock-constants.js
+++ src/mocks/mock-constants.js
@@ -1,4 +1,4 @@
 // Define a root api endpoint for jest tests, use relative for styleguidist site
-const API_ROOT = navigator.userAgent.includes('jsdom') ? 'http://mavenlink.api.com' : '';
+const API_ROOT = navigator.userAgent.includes('jsdom') ? 'http://localhost' : '';
 
 export default { API_ROOT };
