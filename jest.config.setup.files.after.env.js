42f889405a7eb04fe533afc92d7d6b350fcd30fa
Greyson Richey <grichey@mavenlink.com>
2021-02-24 12:14:28 -0700

    Make eslint recognize jest globals everywhere

--- jest.config.setup.files.after.env.js
+++ jest.config.setup.files.after.env.js
@@ -1,12 +1,10 @@
 import jestServer from './src/mocks/jest-server.js';
 
-/* eslint-disable no-undef */
 beforeAll(() => jestServer.listen({
   onUnhandledRequest: 'error',
 }));
 afterEach(() => jestServer.resetHandlers());
 afterAll(() => jestServer.close());
-/* eslint-enable no-undef */
 
 //   During test development, it might be useful to comment out this error handling. It is useful for ensuring our tests
 //   do not violate propTypes but it can make stack traces much harder to understand.
