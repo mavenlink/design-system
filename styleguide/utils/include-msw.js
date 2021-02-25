2532ffa3f2a1dbf7ac1549e523e5cf3cde067760
Greyson Richey <grichey@mavenlink.com>
2021-02-24 17:01:23 -0700

    Dynamically find the mockServiceWorker file based on path
    Co-authored-by: Juan-Carlos Medina <juanca@mavenlink.com>

--- styleguide/utils/include-msw.js
+++ styleguide/utils/include-msw.js
@@ -2,4 +2,8 @@ import { setupWorker } from 'msw';
 import listHandlers from '../../src/components/list-option/mock-handlers.js';
 import customFieldSingleHandlers from '../../src/components/custom-field-input-single-choice/mock-handlers.js';
 
-setupWorker(...listHandlers, ...customFieldSingleHandlers).start();
+setupWorker(...listHandlers, ...customFieldSingleHandlers).start({
+  serviceWorker: {
+    url: `${window.location.pathname}mockServiceWorker.js`,
+  },
+});
