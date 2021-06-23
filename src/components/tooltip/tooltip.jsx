fa8ed26276d723cbe6fba6500f2aeacc85ad4280
Chris Bacon <cbacon@mavenlink.com>
2021-06-23 08:08:57 -0600

    docs(tooltip): add jsdoc for tooltip

--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './tooltip.css';
 
+/**
+ * A component used to render help or reminder text.
+ * @param children The component that needs to be hovered or focused to display the tooltip. These children are where the tooltip "appears" from.
+ * @param className An optional className for the tooltip to provide styling.
+ * @param direction The direction the tooltip should appear from.
+ * @param disabled Whether or not the tooltip should appear at all. If this is true, the tooltip will not appear even when the children receive focus.
+ * @param id The id of the element this tooltip is describing. The element with this id should have have <code>aria-describedby={`${id}-tooltip`}</code>.
+ * @param text The text inside of the tooltip. Tooltip text should be concise and kept to 1-2 short sentences.
+ * @param truncate Whether or not to truncate the <code>text</code> provided. Usage of this prop is generally not recommended unless the text is user-defined.
+ */
 export default function Tooltip({
   children,
   className,
