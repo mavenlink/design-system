5d6dc564b377a598f48e782708528f640316a391
Juan-Carlos Medina <juanca@mavenlink.com>
2021-07-07 15:06:16 -0700

    feat(Input): apply similar styles from Input form control

--- src/components/cell-control/input.jsx
+++ src/components/cell-control/input.jsx
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import CellControl from '../cell-control/cell-control.jsx';
+import styles from './input.css';
 
 function Input(props) {
+  const classNames = {
+    input: styles.input,
+  };
+
   return (
     <CellControl
       labelledBy={props.labelledBy}
     >
-      <input />
+      <input
+        className={classNames.input}
+      />
     </CellControl>
   );
 }
