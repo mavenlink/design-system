c598d71311cc51755eaaf0c6316a69dcdfa39c7d
Juan-Carlos Medina <juanca@mavenlink.com>
2022-02-23 11:29:42 -0600

    fix(analytics): disable analytics in CI environments

--- styleguide/content/utils/page-view.js
+++ styleguide/content/utils/page-view.js
@@ -3,5 +3,5 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-7529271-31');
 
 export default function pageView() {
-  ReactGA.pageview(window.location.hash);
+  if (!window.CI) ReactGA.pageview(window.location.hash);
 }
