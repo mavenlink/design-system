14bfe1c93a99250ce89eaccdc18492923f05a3df
Juan-Carlos Medina <juanca@mavenlink.com>
2022-03-17 11:30:30 -0600

    Revert "fix(multi-autocompleter): autocompleter endpoint is relative to window's host"
    This reverts commit c2f64d39261f1267e35c8425313436f56eac87fb.

--- src/components/control/multi-autocompleter.jsx
+++ src/components/control/multi-autocompleter.jsx
@@ -2,9 +2,10 @@ import PropTypes from 'prop-types';
 import React, { forwardRef, useEffect, useState, useRef } from 'react';
 import useFetch from '@bloodyaugust/use-fetch';
 import MultiSelect from '../control/multi-select.jsx';
+import { API_ROOT } from '../../mocks/mock-constants.js';
 
 function generateUrl(apiEndpoint, [key, value]) {
-  const url = new URL(`api/v1${apiEndpoint}`, `${window.location.protocol}//${window.location.host}`);
+  const url = new URL(`${API_ROOT}${apiEndpoint}`);
   url.searchParams.append(key, value);
   return url.toString();
 }
