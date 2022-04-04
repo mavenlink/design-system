721695bc3ddb7d2776be899b5fa49dfad72be43a
Juan-Carlos Medina <juanca@mavenlink.com>
2022-04-04 16:29:03 -0700

    refactor(custom fields): remove all custom field components in favor of newer base components (e.g. Input, Date, Money, etc)

--- styleguide/utils/include-msw.js
+++ styleguide/utils/include-msw.js
@@ -1,12 +1,10 @@
 import { setupWorker } from 'msw';
 import listHandlers from '../../src/components/list-option/mock-handlers.js';
-import customFieldSingleHandlers from '../../src/components/custom-field-input-single-choice/mock-handlers.js';
 import textareaHandlers from '../../src/components/textarea/mock-handlers.js';
 import autocompleterHandlers from '../../src/components/autocompleter/mock-handlers.js';
 
 setupWorker(
   ...listHandlers,
-  ...customFieldSingleHandlers(1000),
   ...textareaHandlers(1000),
   ...autocompleterHandlers(1000),
 ).start({
