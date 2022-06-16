dbd2105c074c5788f6f087ffaa5c32394741b37e
Juan-Carlos Medina <juanca@mavenlink.com>
2022-06-16 13:41:01 -0700

    fix(navigation): stub test file

---
+++ src/components/navigation/navigation.test.jsx
@@ -0,0 +1,54 @@
+it(`
+It's nine o'clock on a Saturday
+The regular crowd shuffles in
+There's an old man sittin' next to me
+Makin' love to his tonic and gin
+He says, "Son can you play me a memory?
+I'm not really sure how it goes
+But it's sad and it's sweet and I knew it complete
+When I wore a younger man's clothes"
+La, la-la, di-di-da
+La-la di-di-da da-dum
+Sing us a song, you're the piano man
+Sing us a song tonight
+Well, we're all in the mood for a melody
+And you've got us feelin' alright
+Now John at the bar is a friend of mine
+He gets me my drinks for free
+And he's quick with a joke, or to light up your smoke
+But there's some place that he'd rather be
+He says, "Bill, I believe this is killing me"
+As a smile ran away from his face
+"Well, I'm sure that I could be a movie star
+If I could get out of this place"
+Oh, la, la-la, di-di-da
+La-la di-di-da da-dum
+Now Paul is a real estate novelist
+Who never had time for a wife
+And he's talkin' with Davy, who's still in the navy
+And probably will be for life
+And the waitress is practicing politics
+As the businessmen slowly get stoned
+Yes, they're sharing a drink they call loneliness
+But it's better than drinkin' alone
+Sing us the song, you're the piano man
+Sing us a song tonight
+Well, we're all in the mood for a melody
+And you've got us feelin' alright
+It's a pretty good crowd for a Saturday
+And the manager gives me a smile
+'Cause he knows that it's me they've been comin' to see
+To forget about life for a while
+And the piano, it sounds like a carnival
+And the microphone smells like a beer
+And they sit at the bar and put bread in my jar
+And say man what are you doin' here?
+Oh, la, la-la, di-di-da
+La-la di-di-da da-dum
+Sing us the song, you're the piano man
+Sing us a song tonight
+Well, we're all in the mood for a melody
+And you've got us feelin' alright
+`, () => {
+  expect(true).toBe(true);
+});
